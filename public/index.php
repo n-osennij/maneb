b2ac0ae19de97e153463a5abe223ff5ebce5b22c
n-osennij <n.osennij@gmail.com>
2022-05-25 15:09:30 +0300

    add some blocks

---
+++ public/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MANEB</title>
+    <link href="css/app.min.css" rel="stylesheet">
+</head>
+<body class="language-php h-full w-full font-sans text-gray-900 antialiased">
+
+    <!-- NAV -->
+    <?php include '__nav.php' ?>
+
+    <?php include '__hero.php' ?>
+
+    <?php include '__features.php' ?>
+
+    <!-- FOOTER -->
+    <?php include '__footer.php' ?>
+
+    <scrypt src="js/app.min.js"></scrypt>
+</body>
+</html>
